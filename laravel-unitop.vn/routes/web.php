a5c9d1ce593614953050abd124c2ab2764e06458
lethanhbinhxq <hgutnj@gmail.com>
2026-03-04 15:24:43 +0700

    Study parent - child layout view

--- laravel-unitop.vn/routes/web.php
+++ laravel-unitop.vn/routes/web.php
@@ -13,9 +13,13 @@ Route::get('/', function () {
 // Route::get('/product/create', [ProductController::class, 'create']);
 // Route::get('/product/update/{id}', [ProductController::class, 'update']);
 
-Route::resource('post', PostController::class);
+// Route::resource('post', PostController::class);
 
-Route::get('admin/post/create', [AdminPostController::class, 'create']);
-Route::get('admin/post/show/{id?}', [AdminPostController::class, 'show']);
-Route::get('admin/post/update/{id}', [AdminPostController::class, 'update']);
-Route::get('admin/post/delete/{id}', [AdminPostController::class, 'delete']);
+// Route::get('admin/post/create', [AdminPostController::class, 'create']);
+// Route::get('admin/post/show/{id?}', [AdminPostController::class, 'show']);
+// Route::get('admin/post/update/{id}', [AdminPostController::class, 'update']);
+// Route::get('admin/post/delete/{id}', [AdminPostController::class, 'delete']);
+
+Route::get('child', function() {
+    return view('child');
+});
