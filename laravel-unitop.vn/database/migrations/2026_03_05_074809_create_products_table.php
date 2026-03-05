47f5871ef6434677097a5ef48a74b72a7083c3df
lethanhbinhxq <hgutnj@gmail.com>
2026-03-05 14:49:49 +0700

    Do exercise migragtion

--- laravel-unitop.vn/database/migrations/2026_03_05_074809_create_products_table.php
+++ laravel-unitop.vn/database/migrations/2026_03_05_074809_create_products_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 200);
+            $table-> text('content');
+            $table->unsignedInteger('price');
+            $table->unsignedBigInteger('product_cat_id');
+            $table->foreign('product_cat_id')->references('id')->on('product_cat')->onDelete('cascade');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
