671bf90ef3f570763fa4a7bb30f52056fe08efef
lethanhbinhxq <hgutnj@gmail.com>
2026-03-05 14:32:45 +0700

    Add foreign key

--- laravel-unitop.vn/database/migrations/2026_03_05_061935_create_posts_table.php
+++ laravel-unitop.vn/database/migrations/2026_03_05_061935_create_posts_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
             $table->id();
             $table->string('title', 200);
             $table->text('content')->nullable();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            
             $table->timestamps();
         });
     }
