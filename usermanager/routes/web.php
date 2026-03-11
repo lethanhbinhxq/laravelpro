8b79a86fdf511447ab634e3bd36056919a3f9236
lethanhbinhxq <hgutnj@gmail.com>
2026-03-11 15:14:00 +0700

    Add email verification

--- usermanager/routes/web.php
+++ usermanager/routes/web.php
@@ -15,6 +15,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+})->middleware(['auth', 'verified']);
+
+Route::get('/email/verify', function () {
+    return view('auth.verify-email');
+})->middleware('auth')->name('verification.notice');
 
 require __DIR__.'/auth.php';
