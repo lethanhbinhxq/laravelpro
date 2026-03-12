d3986d1773da743b2d1578e34aba82ab2ff6242d
lethanhbinhxq <hgutnj@gmail.com>
2026-03-12 14:58:58 +0700

    Install auth for admin project

--- ltbeauty/routes/web.php
+++ ltbeauty/routes/web.php
@@ -1,36 +1,20 @@
 <?php
 
-use App\Http\Controllers\AdminController;
-use App\Http\Controllers\OrderController;
-use App\Http\Controllers\PageController;
-use App\Http\Controllers\PermissionController;
-use App\Http\Controllers\PostController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/admin', [AdminController::class, 'dashboard'])->name('admin');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('admin/order', [OrderController::class, 'show'])->name('admin.order');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::get('admin/page', [PageController::class, 'show'])->name('admin.page');
-Route::get('admin/page/add', [PageController::class, 'add'])->name('admin.page.add');
-
-Route::get('admin/permission', [PermissionController::class, 'permission']);
-Route::get('admin/role', [PermissionController::class, 'showRole']);
-Route::get('admin/role/add', [PermissionController::class, 'addRole']);
-
-Route::get('admin/post', [PostController::class, 'show']);
-Route::get('admin/post/add', [PostController::class, 'add']);
-Route::get('admin/post/cat', [PostController::class, 'cat']);
-
-Route::get('admin/product', [ProductController::class, 'show']);
-Route::get('admin/product/add', [ProductController::class, 'add']);
-Route::get('admin/product/cat', [ProductController::class, 'cat']);
-
-Route::get('admin/user', [UserController::class, 'show']);
-Route::get('admin/user/add', [UserController::class, 'add']);
+require __DIR__.'/auth.php';
