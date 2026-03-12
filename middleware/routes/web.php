49d1e1c8b7b75d61b2fe2f165045f0cf0a307b33
lethanhbinhxq <hgutnj@gmail.com>
2026-03-12 09:30:41 +0700

    Add middleware in group

--- middleware/routes/web.php
+++ middleware/routes/web.php
@@ -16,8 +16,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('admin/dashboard', [AdminController::class, 'dashboard'])->middleware('CheckRole:Subscriber');
 });
 
-Route::get('admin/dashboard', [AdminController::class, 'dashboard'])->middleware('auth', 'CheckRole:Subscriber');
+// Route::get('admin/dashboard', [AdminController::class, 'dashboard'])->middleware('auth', 'CheckRole:Subscriber');
 
 require __DIR__.'/auth.php';
