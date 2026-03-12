03ad2f6afdd83d7a7d34bbedd1d230700380c6f5
lethanhbinhxq <hgutnj@gmail.com>
2026-03-12 16:41:22 +0700

    Add home and register shortcut

--- ltbeauty/resources/views/auth/login.blade.php
+++ ltbeauty/resources/views/auth/login.blade.php
@@ -32,7 +32,7 @@
             </label>
         </div>
 
-        <div class="flex items-center justify-end mt-4">
+        <div class="flex items-center justify-end mt-2">
             @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
@@ -42,6 +42,12 @@
             <x-primary-button class="ms-3">
                 {{ __('Log in') }}
             </x-primary-button>
+        </div>
+
+        <div class="flex justify-center mt-4">
+            <a class="underline text-sm text-center text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
+                {{ __('Chưa có tài khoản? Đăng ký ngay') }}
+            </a>
         </div>
     </form>
 </x-guest-layout>
--- ltbeauty/resources/views/layouts/guest.blade.php
+++ ltbeauty/resources/views/layouts/guest.blade.php
@@ -20,6 +20,8 @@
                 <a href="/">
                     <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
                 </a>
+
+                <a href="/">Về trang chủ</a>
             </div>
 
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
